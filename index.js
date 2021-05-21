4d595eb5f3f44592f495748119c5d9e62472ffd5
ubuntu <ubuntu@localhost.localdomain>
2021-05-21 18:29:21 +0800

    ok

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const path = require('path');
 const serve = require('koa-static');
 const route = require('koa-route');
 const axios = require('axios');
+const qs = require('qs');
 
 const app = new Koa();
 
@@ -17,16 +18,35 @@ const oauth = async ctx => {
   const requestToken = ctx.request.query.code;
   console.log('authorization code:', requestToken);
 
-  const tokenResponse = await axios({
-    method: 'post',
-    url: 'https://arkid.demo.longguikeji.com/oauth/token/?' +
-      `client_id=${clientID}&` +
-      `client_secret=${clientSecret}&` +
-      `code=${requestToken}`,
-    headers: {
-      accept: 'application/json'
-    }
+  const data = qs.stringify({
+    'state': 'xyz',
+    'client_id': 'abcn82LQg9kXrVsI7CC0QG2Zap6SM6K4REsHQOl0',
+    'client_secret': 'ZDMmLZ7dsMacHhnK8AQLjiKwCb4joFkx2FylADePGKk56nbOJ1zuEyObhiyKLHiGz9dkq3oGLFKrn70dqvc9Zb28ECwovP0WA6TG0SofjO5SwXncsmNoSAOybpgd3Zy4',
+    'grant_type': 'authorization_code',
+    'code': `${requestToken}`
   });
+  const config = {
+    method: 'post',
+    url: 'https://arkid.demo.longguikeji.com/oauth/token/',
+    headers: { 
+      'Authorization': 'Basic YWJjbjgyTFFnOWtYclZzSTdDQzBRRzJaYXA2U002SzRSRXNIUU9sMDpaRE1tTFo3ZHNNYWNIaG5LOEFRTGppS3dDYjRqb0ZreDJGeWxBRGVQR0trNTZuYk9KMXp1RXlPYmhpeUtMSGlHejlka3Ezb0dMRktybjcwZHF2YzlaYjI4RUN3b3ZQMFdBNlRHMFNvZmpPNVN3WG5jc21Ob1NBT3licGdkM1p5NA==', 
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    data : data
+  };
+  
+  const tokenResponse = await axios(config);
+
+	//const tokenResponse = await axios({
+  	//  method: 'post',
+  	//  url: 'https://arkid.demo.longguikeji.com/oauth/token/?' +
+  	//    `client_id=${clientID}&` +
+  	//    `client_secret=${clientSecret}&` +
+  	//    `code=${requestToken}`,
+  	//  headers: {
+  	//    accept: 'application/json'
+  	//  }
+  	//});
 
   const accessToken = tokenResponse.data.access_token;
   console.log(`access token: ${accessToken}`);
@@ -36,11 +56,11 @@ const oauth = async ctx => {
     url: `https://arkid.demo.longguikeji.com/oauth/userinfo/`,
     headers: {
       accept: 'application/json',
-      Authorization: `token ${accessToken}`
+      Authorization: `Bearer ${accessToken}`
     }
   });
   console.log(result.data);
-  const name = result.data.name;
+  const name = result.data.data.user.name;
 
   ctx.response.redirect(`/welcome.html?name=${name}`);
 };
@@ -48,4 +68,4 @@ const oauth = async ctx => {
 app.use(main);
 app.use(route.get('/oauth/redirect', oauth));
 
-app.listen(8080);
+app.listen(8081);
